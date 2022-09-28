8a09ced9396d6d57f11a740cd0ad4cf7d585eab4
Evren Keskin <evrenkeskin0998@gmail.com>
2022-09-29 01:45:20 +0300

    week_1 added
    week 1 answers added.

---
+++ Week_1/Week1_Lab_Q1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	/*
+	printf("hello world");
+	*/
+	
+	
+	int a, b;
+	printf("enter any interger for a and b: \n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("a: %d and b: %d", a, b);
+	
+}
